6af22313abf0dcac15e7fdc570aa9fde3e9929df
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2025-04-02 14:28:16 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className='h-screen bg-[#FFE6D5] grid place-content-center'>
+      <h1 className='text-[#58551E]'>Mumbai Bookies QR Check-in</h1>
+    </div>
+  )
+}
+
+export default App
